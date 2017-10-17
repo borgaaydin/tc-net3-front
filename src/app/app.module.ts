ce921cc89259bedfc2b533d6e94fe1caaf705278
borga <borgaaydin@gmail.com>
2017-10-17 16:59:29 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 
+import { routing } from './app.routes';
+
+import { customHttpProvider } from './injectables/helpers/index';
+import { AlertComponent } from './components/alert/index';
+import { AuthGuard } from './injectables/guards/index';
+import { AlertService, AuthenticationService, UserService } from './services/index';
+import { HomeComponent } from './components/home/index';
+import { LoginComponent } from './components/login/index';
+import { RegisterComponent } from './components/register/index';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  providers: [
+    customHttpProvider,
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
