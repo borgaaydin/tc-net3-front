9674390886a9bd4d8302417aef94098383423557
borga <borgaaydin@gmail.com>
2017-11-20 22:21:07 +0100

    auth guard fixed

--- src/app/injectables/guards/auth.guard.ts
+++ src/app/injectables/guards/auth.guard.ts
@@ -1,27 +1,19 @@
 ﻿import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import decode from 'jwt-decode';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private router: Router) { }
+  constructor(private router: Router) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const expectedRole = route.data.expectedRole;
-        const token = localStorage.getItem('currentUser');
-        const tokenPayload = decode(token);
-        if (token) {
-            if (tokenPayload.scope === 'ens') {
-              // logged in as teacher
-
-            }
-            // logged in so return true
-            return true;
-        }
-
-        // not logged in so redirect to login page with the return url
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
-        return false;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (localStorage.getItem('currentUser')) {
+      // logged in so return true
+      return true;
     }
+
+    // not logged in so redirect to login page with the return url
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+    return false;
+  }
 }
